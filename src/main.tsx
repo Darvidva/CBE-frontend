50a4579745f229ce9585059d95b7b58856287777
Darvidva <mbredavid8@gmail.com>
2025-09-16 12:21:56 +0100

    Removed Maintenance Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 
   import { createRoot } from "react-dom/client";
-  // import App from "./App.tsx";
+  import App from "./App.tsx";
   import "./index.css";
-  import  Maintenance  from "./Maintenance.tsx"
+  // import  Maintenance  from "./Maintenance.tsx"
 
-  // createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(<App />);
 
-  createRoot(document.getElementById("root")!).render(<Maintenance />);
+  // createRoot(document.getElementById("root")!).render(<Maintenance />);
   
